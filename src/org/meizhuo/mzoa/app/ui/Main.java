def222a55965bd47f1ed9fb2f33173590da010c0
Jayin Ton <tonjayin@gmail.com>
2014-03-25 22:56:20 +0800

    init

---
+++ src/org/meizhuo/mzoa/app/ui/Main.java
@@ -0,0 +1,32 @@
+package org.meizhuo.mzoa.app.ui;
+
+import org.meizhuo.mzoa.app.R;
+import org.meizhuo.mzoa.app.R.layout;
+import org.meizhuo.mzoa.app.R.menu;
+
+import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Main extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.acty_main);
+		initData();
+		initLayout();
+	}
+
+	@Override
+	protected void initData() {
+
+	}
+
+	@Override
+	protected void initLayout() {
+	
+	}
+ 
+}
